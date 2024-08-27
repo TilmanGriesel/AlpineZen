2cef50bc25c4bdfa83e825a7c29fa6ca1c3febe3
Tilman Griesel <griesel@me.com>
2024-08-27 21:47:30 +0200

    Implement Windows compatibility

--- pkg/sanitizer/sanitizer.go
+++ pkg/sanitizer/sanitizer.go
@@ -15,31 +15,31 @@ import (
 )
 
 func SanitizeImage(filePath string) error {
-	// Check if filePath is empty
 	if filePath == "" {
 		return fmt.Errorf("file path cannot be empty")
 	}
 
-	// Open file securely
 	file, err := os.Open(filepath.Clean(filePath))
 	if err != nil {
 		return fmt.Errorf("failed to open file: %w", err)
 	}
-	defer file.Close()
 
-	// Verify file size to avoid processing very large files
+	// Verify file size; avoid processing very large files
 	fileInfo, err := file.Stat()
 	if err != nil {
+		file.Close()
 		return fmt.Errorf("failed to get file info: %w", err)
 	}
 
 	const maxSize = 10 * 1024 * 1024 // 10 MB limit
 	if fileInfo.Size() > maxSize {
+		file.Close()
 		return fmt.Errorf("file is too large: %d bytes", fileInfo.Size())
 	}
 
-	// Decode image to ensure it's a valid image format
+	// Ensure it's a valid image format
 	img, format, err := image.Decode(file)
+	file.Close() // Close the file as soon as it's no longer needed
 	if err != nil {
 		return fmt.Errorf("invalid image format or corrupt image: %w", err)
 	}
@@ -49,18 +49,22 @@ func SanitizeImage(filePath string) error {
 		return fmt.Errorf("decoded image is nil")
 	}
 
-	// Create a temporary file path securely
-	tempSanitizedFilePath := filePath + ".sanitized"
-	outFile, err := os.Create(filepath.Clean(tempSanitizedFilePath))
+	// Delete original file
+	if err := os.Remove(filepath.Clean(filePath)); err != nil {
+		return fmt.Errorf("failed to delete the original file: %w", err)
+	}
+
+	// Create sanitized file
+	outFile, err := os.Create(filepath.Clean(filePath))
 	if err != nil {
-		return fmt.Errorf("failed to create sanitized file: %w", err)
+		return fmt.Errorf("failed to create new file: %w", err)
 	}
 	defer outFile.Close()
 
-	// Ensure correct format is used for encoding
+	// Ensure correct encoding
 	switch strings.ToLower(format) {
 	case "jpeg":
-		err = jpeg.Encode(outFile, img, nil) // use default options for JPEG
+		err = jpeg.Encode(outFile, img, nil)
 	case "png":
 		err = png.Encode(outFile, img)
 	default:
@@ -71,19 +75,13 @@ func SanitizeImage(filePath string) error {
 		return fmt.Errorf("failed to re-encode image: %w", err)
 	}
 
-	// Ensure data is written to disk before replacing original file
 	if err := outFile.Sync(); err != nil {
-		return fmt.Errorf("failed to sync sanitized file to disk: %w", err)
-	}
-
-	// Replace original file with sanitized version
-	err = os.Rename(tempSanitizedFilePath, filePath)
-	if err != nil {
-		return fmt.Errorf("failed to replace original image with sanitized version: %w", err)
+		return fmt.Errorf("failed to sync new file to disk: %w", err)
 	}
 
 	return nil
 }
+
 
 func SanitizeArchivePath(dir, target string) (string, error) {
 	// Ref: https://security.snyk.io/research/zip-slip-vulnerability
